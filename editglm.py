902ceae62a6cf8afe917cd63596d99677bc5eb35
aidanellerker <aidan@Aidans-PC>
2023-02-18 04:44:08 -0500

    starttime and stoptime can be edited

--- editglm.py
+++ editglm.py
@@ -1,14 +1,20 @@
 import gridlabd
 
-global parameters
-
 def modify_glm(params):
+    global parameters
     parameters = params
     # TODO make this a variable
     gridlabd.command('R1_12_47_3.glm')
     gridlabd.start('wait')
-    print(gridlabd.get_value('R1-12-47-3_ol_27', 'length'))
+
 
 def on_init(t):
-    gridlabd.set_value('R1-12-47-3_ol_27', 'length', '208.69')
+    #sets timestep by adjusting interval for all recorders
+    
+
+    #sets start and stop time
+    gridlabd.set_global('clock', parameters[1])
+    gridlabd.set_global('starttime', parameters[1])
+    gridlabd.set_global('stoptime', parameters[2])
+
     return True
