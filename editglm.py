8a9b4f9de919366a00aaa3129219832c9953a357
aidanellerker <aidan@Aidans-PC>
2023-02-18 02:34:05 -0500

    Added module to glm, created on_init

--- editglm.py
+++ editglm.py
@@ -1,11 +1,14 @@
 import gridlabd
 
+global parameters
+
 def modify_glm(params):
-
-    
-
+    parameters = params
     # TODO make this a variable
     gridlabd.command('R1_12_47_3.glm')
-
-    print(gridlabd.get_global('starttime'))
     gridlabd.start('wait')
+    print(gridlabd.get_value('R1-12-47-3_ol_27', 'length'))
+
+def on_init(t):
+    gridlabd.set_value('R1-12-47-3_ol_27', 'length', '208.69')
+    return True
